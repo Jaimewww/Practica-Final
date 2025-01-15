9d837c2f0babfe341b758a5fc0e5c94bd6dba710
Jaimewww <jaimelandazuri719@gmail.com>
2025-01-14 23:29:49 -0500

    Primer aporte

---
+++ src/controller/Informe.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+import view.*;
+/**
+ *
+ * @author JAIMEUNL
+ */
+public class Informe {
+    public void agregarDato(String mes, Integer items, Float valor, Float gastos){
+        
+    }
+}
